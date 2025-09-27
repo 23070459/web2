0e35f1b9308c479f523d112054ce2831e26e4aeb
23070459 <23070459@vnu.edu.vn>
2025-09-27 09:15:18 +0700

    w3
    w3

---
+++ w3/ex2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is my first PHP</h1>
+    <p>
+        This is x value:
+        <?php
+            $x = 5;
+            var_dump($x);
+            //echo "The value of x is: " . $x;
+        ?>
+    </p>
+</body>
+</html>
